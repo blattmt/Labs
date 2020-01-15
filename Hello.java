d72457e4bff9fb156dbdf3efabacf417174d4ca6
blattmt <59925198+blattmt@users.noreply.github.com>
2020-01-15 10:00:42 -0500

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,19 @@
+/***********************************
+*Hello
+************************************
+*Michael Blatt
+*15JAN20
+*CSMC 255-003
+***********************************/
+
+public class Hello{
+	public static void main(String[] args) {
+
+		System.out.println("Michael Blatt");
+		System.out.println("Hello");
+		System.out.println("First Lab");
+		System.out.println("15JAN20");
+		System.out.println("CSMC 255-003");
+		System.out.println();
+	}
+}
